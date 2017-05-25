677850c102fd387ff9ddd99079fa0ffc17534885
alexdbaf <drappa.alessandro@gmail.com>
2017-05-25 14:39:01 +0200

    database is ok

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
-  belongs_to :cocktail
+  belongs_to :cocktail, dependent: :destroy
+
+  validates :cocktail_id, uniqueness: { scope: [:ingredient_id] }
 end
